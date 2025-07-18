eee40bbbaca9ba39d7eb7594315938538e2f79f0
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-07-18 09:06:41 +0800

    Update installgames.sh
    make it download from steam_legacy and previous_build branch, instead of 25 anniv update

--- installgames.sh
+++ installgames.sh
@@ -135,13 +135,13 @@ while true; do
 
             for choice in "${choices[@]}"; do
                 case "$choice" in
-                    1) commands+=("-app 220 -depot 221 -dir source") ;;
-                    2) commands+=("-app 220 -depot 389 -dir source") ;;
-                    3) commands+=("-app 220 -depot 380 -dir source") ;;
-                    4) commands+=("-app 220 -depot 420 -dir source") ;;
-                    5) commands+=("-app 220 -depot 380 -dir source") ;;
+                    1) commands+=("-branch steam_legacy -app 220 -depot 221 -dir source") ;;
+                    2) commands+=("-branch steam_legacy -app 220 -depot 389 -dir source") ;;
+                    3) commands+=("-branch steam_legacy -app 220 -depot 380 -dir source") ;;
+                    4) commands+=("-branch steam_legacy -app 220 -depot 420 -dir source") ;;
+                    5) commands+=("-branch steam_legacy -app 220 -depot 380 -dir source") ;;
                     6) commands+=("-app 240 -depot 241 -dir source -branch previous_build") ;;
-                    7) commands+=("-app 300 -depot 301 -dir source") ;;
+                    7) commands+=("-branch previous_build -app 300 -depot 301 -dir source") ;;
                     8) commands+=("-app 400 -depot 401 -dir source") ;;
                     9|10|11|12|13) goldsrc_choices+=("$choice") ;;
                 esac
@@ -179,11 +179,11 @@ while true; do
                         fi
                         if [[ "$manual_version" == "2" || "$manual_version" == "3" ]]; then
                             case "$choice" in
-                                9)  commands+=("-beta steam_legacy -app 70  -depot 1   -dir goldsrc_old") ;;
-                                10) commands+=("-beta steam_legacy -app 130 -depot 130 -dir goldsrc_old") ;;
-                                11) commands+=("-beta steam_legacy -app 50  -depot 51  -dir goldsrc_old") ;;
-                                12) commands+=("-beta steam_legacy -app 10  -depot 11  -dir goldsrc_old") ;;
-                                13) commands+=("-beta steam_legacy -app 20  -depot 21  -dir goldsrc_old") ;;
+                                9)  commands+=("-branch steam_legacy -app 70  -depot 1   -dir goldsrc_old") ;;
+                                10) commands+=("-branch steam_legacy -app 130 -depot 130 -dir goldsrc_old") ;;
+                                11) commands+=("-branch steam_legacy -app 50  -depot 51  -dir goldsrc_old") ;;
+                                12) commands+=("-branch steam_legacy -app 10  -depot 11  -dir goldsrc_old") ;;
+                                13) commands+=("-branch steam_legacy -app 20  -depot 21  -dir goldsrc_old") ;;
                             esac
                         fi
                     done
