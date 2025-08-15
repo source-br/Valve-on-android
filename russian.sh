3db6a9bc22a2eeabb0b77f3ac480510b4d5f0717
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-08-15 18:34:03 +0800

    Update russian.sh
    translate untranslated strings

--- russian.sh
+++ russian.sh
@@ -1,73 +1,73 @@
-# Depot
-LANG_DEPOT="DepotDownloader is required but not found."
-LANG_TRY_INSTALL_DEPOT="The script will attempt to install TermuxDepotDownloader in 5 seconds..."
+# DepotDownloader
+LANG_DEPOT="DepotDownloader нужен но не найден."
+LANG_TRY_INSTALL_DEPOT="Скрипт попытается установить TermuxDepotDownloader через 5 секунд..."
 
-# Main screen
+# Main screen - главное меню
 LANG_MAIN_OPTION_ALL="Все игры"
 LANG_MAIN_OPTION_MANUAL="Выбрать вручную"
 
-# All games
+# All games - все игры
 LANG_DOWNLOADS_OPTION_ALL_SOURCE="Все Source игры"
 LANG_DOWNLOADS_OPTION_ALL_GOLDSRC="Все Goldsrc игры"
 
-# Manual
+# Manual - маунальное
 LANG_GAMES_TITLE_SOURCE="Source игры:"
 LANG_GAMES_TITLE_GOLDSRC="Goldsrc игры:"
-LANG_PROMPT_CHOOSE_MORE="Выберите одну или больше опций, разделенные запятыми, например:1,3"
+LANG_PROMPT_CHOOSE_MORE="Выберите одну или больше опций, разделенные запятыми, например: 1,3"
 
 # Goldsrc
-LANG_GOLDSRCVERSION_TITLE="Which version of the Goldsrc games do you want?"
-LANG_GOLDSRCVERSION_OPTION_25TH="Latest version"
-LANG_GOLDSRCVERSION_OPTION_PRE25TH="Pre-25th Anniversary version"
+LANG_GOLDSRCVERSION_TITLE="Какую версию игр GoldSrc игр вы хотите установить?"
+LANG_GOLDSRCVERSION_OPTION_25TH="Последняя версия"
+LANG_GOLDSRCVERSION_OPTION_PRE25TH="Версия до 25 летней версия"
 LANG_WARNING_OLD_VERSION="[!] Внимание: Старая версия, лучше для Xash3D-FWGS (Старый движок)"
 
-# Select languages
-LANG_ASK_LANGUAGE_PACKS="Do you want to download additional language packs?"
-LANG_SELECT_LANGUAGE_PACK="Select the language pack you want to include:"
-LANG_SELECT_COMMUNITY_LANGUAGE="What type of translation pack?"
+# Select languages - Выбор языка игры
+LANG_ASK_LANGUAGE_PACKS="Вы хотите установить языковые паки для игр?"
+LANG_SELECT_LANGUAGE_PACK="Выберите языковые паки которые должны быть скачаны вместе с игрой:"
+LANG_SELECT_COMMUNITY_LANGUAGE="Какой тип языковых паков вы хотите?"
 
-# Languages
-LANG_ENGLISH="English (English)"
-LANG_SPANISH_E="Español España (Spanish Spain)"
-LANG_SPANISH_L="Español Latinoamérica (Spanish Latin America)" 
-LANG_THAI="ไทย (Thai)"
-LANG_FRENCH="Français (French)"
-LANG_GERMAN="Deutsch (German)"
+# Languages - Языки
+LANG_ENGLISH="English (Английский)"
+LANG_SPANISH_E="Español España (Испанский)"
+LANG_SPANISH_L="Español Latinoamérica (Испанский - латинская Америка)" 
+LANG_THAI="ไทย (Тайский)"
+LANG_FRENCH="Français (Французкий)"
+LANG_GERMAN="Deutsch (Германский)"
 LANG_RUSSIAN="Русский (Russian)"
-LANG_KOREAN="한국어 (Korean)"
-LANG_TCHINESE="繁體中文 (Traditional Chinese)"
-LANG_SCHINESE="简体中文 (Simplified Chinese)"
-LANG_ITALIAN="Italiano (Italian)"
-LANG_JAPANESE="日本語 (Japanese)"
-LANG_PORTUGUESE_BRAZIL="Português Brasil (Portuguese Brazil)"
-LANG_TRANSLATION_TYPE="Which source do you want to download the language packs from?"
-LANG_TRANSLATION_OFFICIAL="Official"
-LANG_TRANSLATION_COMMUNITY="Community"
+LANG_KOREAN="한국어 (Корейский)"
+LANG_TCHINESE="繁體中文 (Китайский - традиционный)"
+LANG_SCHINESE="简体中文 (Китайский - упрощенный)"
+LANG_ITALIAN="Italiano (Итальянский)"
+LANG_JAPANESE="日本語 (Японский)"
+LANG_PORTUGUESE_BRAZIL="Português Brasil (Португальский - Бразилия)"
+LANG_TRANSLATION_TYPE="Какой тип языковых паков для игр Source вы хотите?"
+LANG_TRANSLATION_OFFICIAL="Офицальный"
+LANG_TRANSLATION_COMMUNITY="Из сообщества"
 
-# Login
+# Login - Логин
 LANG_ENTER_USERNAME="Напишите ваш Steam юзернейм:"
 LANG_ENTER_PASSWORD="Напишите ваш Steam пароль:"
 
-# Generic options
-LANG_NO="No"
-LANG_YES="Yes"
+# Generic options - Обычные опции
+LANG_NO="Нет"
+LANG_YES="Да"
 LANG_EXIT="Выйти"
 LANG_BOTH="Оба"
 LANG_ERROR="Ошибка"
-LANG_EXITING="Exiting..."
-LANG_DOWNLOADING="Downloading:"
-LANG_INSTALLING="Installing:"
+LANG_EXITING="Выходим..."
+LANG_DOWNLOADING="Скачиваем:"
+LANG_INSTALLING="Устанавливаем игру:"
 LANG_INVALID_OPTION="Неверная опция."
-LANG_PROMPT_CHOOSE="Choose an option"
-LANG_SUCCESS="was successfully installed"
+LANG_PROMPT_CHOOSE="Выберите опцию"
+LANG_SUCCESS="был успешно установлен"
 LANG_TITLE="Какие игры вы хотите установить?"
-LANG_OPTION_BACK="Back to main menu"
+LANG_OPTION_BACK="Назад к главному меню"
 LANG_FAILED="Не удалось запустить команду выше."
 LANG_PRESS_ENTER="Нажмите ENTER чтобы вернутся в главное меню..."
 LANG_NO_COMMANDS="Команды не выбраны. Возращаемся в меню..."
 LANG_COMMANDS_ABOVE="Не удалось запустить команду выше."
 LANG_NO_GAMES="Игры не выбранны. Возращаемся в главное меню..."
-LANG_DOWNLOADING_LANG_PACK="Downloading language pack:"
-LANG_BY_SRBR_MPD="${BLUE}[${RESET}${GREEN}*${RESET}${BLUE}]${RESET} By ${GREEN}Source BR${RESET} and Mesa Preta Dubs"
-LANG_FAILED_DOWNLOAD="Failed to download :("
-LANG_SUCCESS_DOWNLOAD="Downloaded successfully :)"
+LANG_DOWNLOADING_LANG_PACK="Устанавливаем языковой пак:"
+LANG_BY_SRBR_MPD="${BLUE}[${RESET}${GREEN}*${RESET}${BLUE}]${RESET} Создан благадоря ${GREEN}Source BR${RESET} и Mesa Preta Dubs"
+LANG_FAILED_DOWNLOAD="Не удалось скачать файлы :("
+LANG_SUCCESS_DOWNLOAD="Файлы скачались успешно :)"
